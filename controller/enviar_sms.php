a8a3ac22b3f2497e794d14649720946e28b36084
NeoRazorX <neorazorx@gmail.com>
2016-02-12 20:45:44 +0100

    Corregido facturascripts.ini y enviar_sms.

--- controller/enviar_sms.php
+++ controller/enviar_sms.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of FacturaSctipts
- * Copyright (C) 2015   Luis Miguel Pérez Romero   luismipr@gmail.com
+ * Copyright (C) 2016   Luis Miguel Pérez Romero   luismipr@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -19,7 +19,6 @@
  * 
  */
 
-
 require_model('proveedor_sms.php');
 require_model('cliente.php');
 require_model('servicio_cliente.php');
@@ -33,114 +32,119 @@ require_model('detalle_servicio.php');
  */
 class enviar_sms extends fs_controller
 {
+   public $cliente;
+   public $documento;
+   public $documento_url;
+   public $id;
+   public $mensaje;
+   public $provsms;
+   public $telefono;
+   
+   public function __construct()
+   {
+      parent::__construct(__CLASS__, 'Enviar SMS', 'ventas', FALSE, FALSE);
+   }
+   
+   protected function private_core()
+   {
+      $this->provsms = new proveedor_sms();
+      $this->documento = FALSE;
+      $this->documento_url = $this->url();
+      $cliente = new cliente();
+      
+      $servicio = FALSE;
+      if( isset($_REQUEST['servicio']) )
+      {
+         $serv = new servicio_cliente();
+         $servicio = $serv->get($_REQUEST['id']);
+         if($servicio)
+         {
+            $this->cliente = $cliente->get($servicio->codcliente);
+            $this->id = $servicio->idservicio;
+            $this->documento = 'servicio';
+            $this->documento_url = $servicio->url();
+         }
+      }
+      else if( isset($_REQUEST['pedido']) )
+      {
+         $ped = new pedido_cliente();
+         $pedido = $ped->get($_REQUEST['id']);
+         if($pedido)
+         {
+            $this->cliente = $cliente->get($pedido->codcliente);
+            $this->id = $pedido->idpedido;
+            $this->documento = 'pedido';
+            $this->documento_url = $pedido->url();
+         }
+      }
 
-    public $provsms;
-    public $cliente;
-    public $telefono;
-    public $mensaje;
-    public $documento;
-    public $id;
-
-    public function __construct()
-    {
-        parent::__construct(__CLASS__, 'Enviar SMS', 'ventas', FALSE, FALSE);
-    }
-
-    protected function private_core()
-    {
-
-        $this->provsms = new proveedor_sms();
-        $this->documento = FALSE;
-        $cliente = new cliente();
-
-
-
-        if (isset($_GET['servicio']))
-        {
-            $serv = new servicio_cliente();
-            $this->servicio = $serv->get($_REQUEST['id']);
-            if ($this->servicio)
-            {
-                $this->cliente = $cliente->get($this->servicio->codcliente);
-                $this->id = $this->servicio->idservicio;
-                $this->documento = 'srv';
-            }
-        }
-        else if (isset($_GET['pedido']))
-        {
-            $serv = new pedido_cliente();
-            $this->pedido = $serv->get($_REQUEST['id']);
-            if ($this->pedido)
-            {
-                $this->cliente = $cliente->get($this->pedido->codcliente);
-                $this->id = $this->pedido->idpedido;
-                $this->documento = 'ped';
-            }
-        }
-
-        if (isset($_GET['enviar']))
-        {
-
-            $this->provsms = $this->provsms->get($_POST['proveedor']);
+      if( isset($_POST['enviar']) )
+      {
+         $provsms = $this->provsms->get($_POST['proveedor']);
+         if($provsms)
+         {
             $this->telefono = $_POST['telefono'];
             $this->mensaje = $_POST['mensaje'];
-            if ($this->provsms->enviar_sms($this->telefono, $this->mensaje))
+            if($this->provsms->enviar_sms($this->telefono, $this->mensaje))
             {
-                $this->new_message('Mensaje enviado');
-                if (isset($_GET['srv']))
-                {
-                    $serv = new servicio_cliente();
-                    $this->servicio = $serv->get($_REQUEST['srv']);
-                    $this->agrega_detalle();
-                }
+               $this->new_message('Mensaje enviado');
+               
+               if($servicio)
+               {
+                  $this->agrega_detalle($servicio);
+               }
             }
             else
             {
-                $this->new_error_msg('Error al enviar el mensaje');
+               $this->new_error_msg('Error al enviar el mensaje');
             }
-        }
+         }
+         else
+         {
+            $this->new_error_msg('Proveedor de SMS no encontrado.');
+         }
+      }
 
-        $this->share_extensions();
-    }
+      $this->share_extensions();
+   }
 
-    private function share_extensions()
-    {
-        $fsext = new fs_extension();
-        $fsext->name = 'enviar_sms_servicio';
-        $fsext->from = __CLASS__;
-        $fsext->to = 'ventas_servicio';
-        $fsext->type = 'button';
-        $fsext->text = '<span class="glyphicon glyphicon-phone" aria-hidden="true"></span>'
-                . '<span class="hidden-xs">&nbsp; Enviar SMS</span>';
-        $fsext->params = '&servicio=TRUE';
-        $fsext->save();
+   private function share_extensions()
+   {
+      $fsext = new fs_extension();
+      $fsext->name = 'enviar_sms_servicio';
+      $fsext->from = __CLASS__;
+      $fsext->to = 'ventas_servicio';
+      $fsext->type = 'button';
+      $fsext->text = '<span class="glyphicon glyphicon-phone" aria-hidden="true"></span>'
+              . '<span class="hidden-xs">&nbsp; SMS</span>';
+      $fsext->params = '&servicio=TRUE';
+      $fsext->save();
 
-        $fsext2 = new fs_extension();
-        $fsext2->name = 'enviar_sms_pedido';
-        $fsext2->from = __CLASS__;
-        $fsext2->to = 'ventas_pedido';
-        $fsext2->type = 'button';
-        $fsext2->text = '<span class="glyphicon glyphicon-check" aria-hidden="true"></span>'
-                . '<span class="hidden-xs">&nbsp; Pagar...</span>';
-        $fsext2->params = '&pedido=TRUE';
-        $fsext2->save();
-    }
+      $fsext2 = new fs_extension();
+      $fsext2->name = 'enviar_sms_pedido';
+      $fsext2->from = __CLASS__;
+      $fsext2->to = 'ventas_pedido';
+      $fsext2->type = 'button';
+      $fsext2->text = '<span class="glyphicon glyphicon-phone" aria-hidden="true"></span>'
+              . '<span class="hidden-xs">&nbsp; SMS</span>';
+      $fsext2->params = '&pedido=TRUE';
+      $fsext2->save();
+   }
 
-    private function agrega_detalle()
-    {
-        $detalle = new detalle_servicio();
-        $detalle->descripcion = 'SMS enviado correctamente al teléfono: ' . $this->telefono . ' con el texto: ' . $this->mensaje;
-        $detalle->idservicio = $this->servicio->idservicio;
-        $detalle->nick = $this->user->nick;
+   private function agrega_detalle(&$servicio)
+   {
+      $detalle = new detalle_servicio();
+      $detalle->descripcion = 'SMS enviado correctamente al teléfono: ' . $this->telefono . ' con el texto: ' . $this->mensaje;
+      $detalle->idservicio = $servicio->idservicio;
+      $detalle->nick = $this->user->nick;
 
-        if ($detalle->save())
-        {
-            $this->new_message('Detalle guardados correctamente.');
-        }
-        else
-        {
-            $this->new_error_msg('Imposible guardar el detalle.');
-        }
-    }
-
+      if( $detalle->save() )
+      {
+         $this->new_message('Detalle guardados correctamente.');
+      }
+      else
+      {
+         $this->new_error_msg('Imposible guardar el detalle.');
+      }
+   }
 }
--- controller/proveedores_sms.php
+++ controller/proveedores_sms.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of FacturaSctipts
- * Copyright (C) 2015   Luis Miguel Pérez Romero   luismipr@gmail.com
+ * Copyright (C) 2016   Luis Miguel Pérez Romero   luismipr@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
